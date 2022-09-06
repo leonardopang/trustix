556f18a1a80856fb4848ac451109edf30b01b635
Leonardo Pang <leonardopang12@gmail.com>
2022-09-06 12:31:50 -0300

    Add page 404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+<main>
+  <section class="page-404">
+    <div class="container-wrap">
+      <div class="page-404_container">
+        <div class="title-container text-center">
+          <h1 class="">Página não encontrada ou foi removida</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer() ?>
